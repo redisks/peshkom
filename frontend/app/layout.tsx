b70df5de1d29c9ade10ed04a9ab1388f9c7cff62
dragon.starwings@gmail.com <sherstnev.denis.v@gmail.com>
2025-09-13 15:55:27 +0300

    deploy fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -20,7 +20,7 @@ import Link from "next/link";
 import { YMaps } from "@iminside/react-yandex-maps";
 import Head from "next/head";
 import { PointsContext } from "@/context/PointsContext";
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import { IPlace } from "@/lib/types";
 
 const unbounded = Unbounded({
@@ -53,90 +53,92 @@ export default function RootLayout({
         <body
           className={`${inter.variable} ${unbounded.variable} h-screen w-screen bg-background text-light-black overflow-x-hiddenT ${inter.className}`}
         >
-          <YMaps
-            query={{
-              load: "package.full",
-              apikey: process.env.NEXT_PUBLIC_YANDEX_MAPS_API_KEY,
-            }}
-          >
-            <PointsContext.Provider
-              value={{ points: points, setPoints: setPoints }}
+          <Suspense fallback="error">
+            <YMaps
+              query={{
+                load: "package.full",
+                apikey: process.env.NEXT_PUBLIC_YANDEX_MAPS_API_KEY,
+              }}
             >
-              <AuthProvider>
-                <AuthCheck>
-                  <div className="flex justify-center w-full">
-                    {pathname.includes("/auth") ||
-                    pathname.includes("/map") ? null : pathname.includes(
-                        "place/"
-                      ) ? (
-                      <header className="w-full flex justify-between p-5 fixed top-0 z-10">
-                        <div className="p-2 bg-light-white rounded-4xl shadow-md">
-                          <ArrowLeft
-                            className="size-6"
+              <PointsContext.Provider
+                value={{ points: points, setPoints: setPoints }}
+              >
+                <AuthProvider>
+                  <AuthCheck>
+                    <div className="flex justify-center w-full">
+                      {pathname.includes("/auth") ||
+                      pathname.includes("/map") ? null : pathname.includes(
+                          "place/"
+                        ) ? (
+                        <header className="w-full flex justify-between p-5 fixed top-0 z-10">
+                          <div className="p-2 bg-light-white rounded-4xl shadow-md">
+                            <ArrowLeft
+                              className="size-6"
+                              onClick={() => router.back()}
+                            />
+                          </div>
+                          <div className="flex gap-4 items-center">
+                            <div className="p-2 bg-light-white rounded-4xl shadow-md">
+                              <Sparkle className="size-6" />
+                            </div>
+                            <div className="p-2 bg-light-white rounded-4xl shadow-md">
+                              <Share className="size-6" />
+                            </div>
+                          </div>
+                        </header>
+                      ) : (
+                        <header className="w-full bg-light-white flex justify-between p-5 fixed top-0 z-10">
+                          <MoveLeft
+                            className="size-8"
                             onClick={() => router.back()}
                           />
-                        </div>
-                        <div className="flex gap-4 items-center">
-                          <div className="p-2 bg-light-white rounded-4xl shadow-md">
-                            <Sparkle className="size-6" />
-                          </div>
-                          <div className="p-2 bg-light-white rounded-4xl shadow-md">
-                            <Share className="size-6" />
-                          </div>
-                        </div>
-                      </header>
-                    ) : (
-                      <header className="w-full bg-light-white flex justify-between p-5 fixed top-0 z-10">
-                        <MoveLeft
-                          className="size-8"
-                          onClick={() => router.back()}
-                        />
-                        <span className="font-unbounded font-medium text-xl h-8 flex items-center">
-                          PESHKOM
-                        </span>
-                        <CircleUser className="size-8" />
-                      </header>
-                    )}
-                    <main
-                      className={`w-full ${
-                        pathname.includes("place/") ||
-                        pathname.includes("/map") ||
-                        pathname.includes("/auth")
-                          ? "mt-0"
-                          : "mt-20"
-                      }`}
-                    >
-                      {children}
-                    </main>
-                    {pathname.includes("/auth") ||
-                    pathname.includes("/map") ? null : (
-                      <nav
-                        className={`flex justify-between gap-4 items-center mb-5 p-5 w-11/12 max-w-screen fixed bottom-0 bg-white shadow-2xl rounded-3xl z-10 ${
-                          pathname.includes("place/") ? "hidden" : ""
+                          <span className="font-unbounded font-medium text-xl h-8 flex items-center">
+                            PESHKOM
+                          </span>
+                          <CircleUser className="size-8" />
+                        </header>
+                      )}
+                      <main
+                        className={`w-full ${
+                          pathname.includes("place/") ||
+                          pathname.includes("/map") ||
+                          pathname.includes("/auth")
+                            ? "mt-0"
+                            : "mt-20"
                         }`}
                       >
-                        <Link href="/posts">
-                          <PanelTop className="size-8" />
-                        </Link>
-                        <Link href="/">
-                          <MessageCircleHeart className="size-8" />
-                        </Link>
-                        <Link href="/map">
-                          <Map className="size-8" />
-                        </Link>
-                        <Link href="/">
-                          <Focus className="size-8" />
-                        </Link>
-                        <Link href="/">
-                          <CircleUser className="size-8" />
-                        </Link>
-                      </nav>
-                    )}
-                  </div>
-                </AuthCheck>
-              </AuthProvider>
-            </PointsContext.Provider>
-          </YMaps>
+                        {children}
+                      </main>
+                      {pathname.includes("/auth") ||
+                      pathname.includes("/map") ? null : (
+                        <nav
+                          className={`flex justify-between gap-4 items-center mb-5 p-5 w-11/12 max-w-screen fixed bottom-0 bg-white shadow-2xl rounded-3xl z-10 ${
+                            pathname.includes("place/") ? "hidden" : ""
+                          }`}
+                        >
+                          <Link href="/posts">
+                            <PanelTop className="size-8" />
+                          </Link>
+                          <Link href="/">
+                            <MessageCircleHeart className="size-8" />
+                          </Link>
+                          <Link href="/map">
+                            <Map className="size-8" />
+                          </Link>
+                          <Link href="/">
+                            <Focus className="size-8" />
+                          </Link>
+                          <Link href="/">
+                            <CircleUser className="size-8" />
+                          </Link>
+                        </nav>
+                      )}
+                    </div>
+                  </AuthCheck>
+                </AuthProvider>
+              </PointsContext.Provider>
+            </YMaps>
+          </Suspense>
         </body>
       </html>
     </>
